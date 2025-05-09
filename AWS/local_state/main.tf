2cd4ee266f95ba32cd83ba8921f0cd9c9314f324
Kamal Juyal <Kamal Juyal>
2025-05-09 21:45:58 +0530

    TF

--- AWS/local_state/main.tf
+++ AWS/local_state/main.tf
@@ -1,3 +1,14 @@
+terraform {
+    required_providers {
+        aws = {
+           source = "hashicorp/aws"
+           version = "~> 4.16"
+        }
+    }   
+ 
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
@@ -5,4 +16,8 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "TF_demo"
+    }
 }
