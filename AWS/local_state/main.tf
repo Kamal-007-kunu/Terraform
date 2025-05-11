41548e1fc2b148cdd50b0c3edef38b2d630f92c2
Kamal Juyal <Kamal Juyal>
2025-05-11 09:39:56 +0530

    asdas

--- AWS/local_state/main.tf
+++ AWS/local_state/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-southeast-2"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
